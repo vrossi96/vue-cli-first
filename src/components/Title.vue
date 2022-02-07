87df9ca963e661fe0de41c243a3a25b2223c8f1e
vrossi96 <vrossi396@yahoo.it>
2022-02-07 17:22:29 +0100

    add title

---
+++ src/components/Title.vue
@@ -0,0 +1,14 @@
+<template>
+   <div>
+      <h1>La mia prima app con Vue CLI!</h1>
+   </div>
+</template>
+
+<script>
+export default {
+   name: "Title",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style></style>
